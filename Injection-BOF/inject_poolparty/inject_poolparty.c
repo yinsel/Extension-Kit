c4364b7d3656df960c376dd59b8d5cf69628c406
Jacopo Augelli <106108757+jacopoaugelli@users.noreply.github.com>
2025-08-12 20:13:55 +0200

    Update inject_poolparty.c

--- Injection-BOF/inject_poolparty/inject_poolparty.c
+++ Injection-BOF/inject_poolparty/inject_poolparty.c
@@ -19,14 +19,14 @@ void go(char * args, int len) {
     BeaconPrintf(CALLBACK_OUTPUT, "Selected technique: %d", technique);
 
     switch(technique) {
-	case 1: Inject1(pid, shellcode, shellcodeSize); break;
-	case 2: Inject2(pid, shellcode, shellcodeSize); break;
-	case 3: Inject3(pid, shellcode, shellcodeSize); break;
-	case 4: Inject4(pid, shellcode, shellcodeSize); break;
-	case 5: Inject5(pid, shellcode, shellcodeSize); break;
-	case 6: Inject6(pid, shellcode, shellcodeSize); break;
-	case 7: Inject7(pid, shellcode, shellcodeSize); break;
-	case 8: Inject8(pid, shellcode, shellcodeSize); break;
-	default: BeaconPrintf(CALLBACK_OUTPUT, "Invalid technique %d", technique);
+		case 1: Inject1(pid, shellcode, shellcodeSize); break;
+		case 2: Inject2(pid, shellcode, shellcodeSize); break;
+		case 3: Inject3(pid, shellcode, shellcodeSize); break;
+		case 4: Inject4(pid, shellcode, shellcodeSize); break;
+		case 5: Inject5(pid, shellcode, shellcodeSize); break;
+		case 6: Inject6(pid, shellcode, shellcodeSize); break;
+		case 7: Inject7(pid, shellcode, shellcodeSize); break;
+		case 8: Inject8(pid, shellcode, shellcodeSize); break;
+		default: BeaconPrintf(CALLBACK_OUTPUT, "Invalid technique %d", technique);
     }
 }
